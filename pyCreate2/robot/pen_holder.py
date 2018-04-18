b3b74aa50c9a45203bc4f58a7ce86c26383ff413
Jacob Antony <antony@usc.edu>
2018-04-18 16:21:50 -0700

    updated marker mobility

--- pyCreate2/robot/pen_holder.py
+++ pyCreate2/robot/pen_holder.py
@@ -4,11 +4,17 @@ Module to control a pen holder.
 
 from . import Servo
 import math
-
+import odometry
 
 class PenHolder:
-    def __init__(self, number):
+    def __init__(self, number, radius, length, factory):
         self.servo = Servo(number)
+        self.factory = factory.create_c
+        self.arm = radius+length
+        self.odometry = odometry.Odometry()
+        self.length = length
+        self.radius = radius
+
 
     def go_to(self, height):
         """Go to specified target height.
@@ -27,3 +33,25 @@ class PenHolder:
             b (float): blue component (0 to 1)
         """
         input('Press [Enter] after you changed the pen color to ({},{},{})'.format(r, g, b))
+
+    def translate_coords(self, x, y):
+        theta = self.odometry.theta
+        y_ = y-arm*math.sin*(90-theta)
+        x_ = x+arm*math.cos*(90-theta)
+        return x_,y_
+
+    def rotate_around_marker(base_speed, angle):
+        wb = self.radius*2
+        ratio = self.length/(self.length + wb)
+        rw_speed = base_speed*ratio
+        lw_speed = base_speed
+        sector = angle/(2*math.pi)
+        circle_time = (2*math.pi)*(self.length + wb)/base_speed
+        sector_time = circle_time * sector
+        return rw_speed, lw_speed, sector_time
+        
+
+
+
+
+
