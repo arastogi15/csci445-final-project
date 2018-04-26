c79742edc6ed241396fc8b69c9503dfb5c6692d0
Ankur Rastogi <ankurras@usc.edu>
2018-04-25 17:49:49 -0700

    sensor fusion working

--- lab11_tracking_test.py
+++ lab11_tracking_test.py
@@ -4,17 +4,63 @@ Use "python3 run.py --sim lab11_tracking_test" to execute
 """
 
 import math
+import math
+import odometry
+from pyCreate2 import create2
 
 
 class Run:
     def __init__(self, factory):
+        self.create = factory.create_create()
         self.time = factory.create_time_helper()
+        self.odometry = odometry.Odometry()
+
+        self.base_speed = 100
+
+
         # sd_{x,y,theta} and rate are only for simulation to change the noise and update rate respectively.
         # They are ignored on the robot.
         self.tracker = factory.create_tracker(1, sd_x=0.01, sd_y=0.01, sd_theta=0.01, rate=10)
 
+    def fusePredict(self,odometry_data, alpha):
+        predict_x = odometry_data.x
+        predict_y = odometry_data.y
+        predict_theta = odometry_data.theta
+
+        r = self.tracker.query()
+
+        if r is not None:
+            predict_x = alpha*predict_x + (1-alpha)*(r["position"]["x"])
+            predict_y = alpha*predict_y + (1-alpha)*(r["position"]["y"])
+            predict_theta = alpha*predict_theta + (1-alpha)*(r["orientation"]["y"]) # given in radians...
+            self.time.sleep(0.0)
+
+        # Values can be found using variable_name["x"]
+        #                           variable_name["y"]
+        #                           variable_name["theta"]
+        return {
+            'x': predict_x,
+            'y': predict_y,
+            'theta': predict_theta
+      }     
+
     def run(self):
+
+        self.create.start()
+        self.create.safe()
+
+        # request sensors
+        self.create.start_stream([
+            create2.Sensor.LeftEncoderCounts,
+            create2.Sensor.RightEncoderCounts,
+        ])
+
         while True:
+            state = self.create.update()
+            if state is not None:
+                self.odometry.update(state.leftEncoderCounts, state.rightEncoderCounts)
+
+            print("BEFORE")
             r = self.tracker.query()
             if r is not None:
                 x = r["position"]["x"]
@@ -23,4 +69,20 @@ class Run:
                 yaw = r["orientation"]["y"]
                 print(x,y,z,math.degrees(yaw))
 
-            self.time.sleep(0.0)
+            self.create.drive_direct(self.base_speed, self.base_speed)
+            self.time.sleep(3.0)
+            self.create.drive_direct(self.base_speed, -1*self.base_speed)
+            self.time.sleep(1.8);
+            r = self.tracker.query()
+            if r is not None:
+                x = r["position"]["x"]
+                y = r["position"]["y"]
+                z = r["position"]["z"]
+                yaw = r["orientation"]["y"]
+                print(x,y,z,math.degrees(yaw))
+            print("AFTER")
+            p = self.fusePredict(self.odometry, 0.5)
+            print(p["x"],p["y"],math.degrees(p["theta"]))
+            print()
+
+
